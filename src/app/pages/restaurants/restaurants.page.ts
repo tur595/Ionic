518e7f88f2568b8cb3dbe65db0b004ed3eea5b69
Sinan Senevrensel <tur595@hotmail.com>
2023-04-17 19:33:09 +0300

    setup for api calls

--- src/app/pages/restaurants/restaurants.page.ts
+++ src/app/pages/restaurants/restaurants.page.ts
@@ -2,19 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { RestaurantsService } from 'src/app/services/restaurants.service';
 
 @Component({
   selector: 'app-restaurants',
   templateUrl: './restaurants.page.html',
   styleUrls: ['./restaurants.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class RestaurantsPage implements OnInit {
+  constructor(private restaurantsService: RestaurantsService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
---
+++ src/app/services/restaurants.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RestaurantsService {
+  constructor(private http: HttpClient) {}
+
+  getCloseRestaurants(): Observable<any> {
+    return this.http.get('');
+  }
+}
